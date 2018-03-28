9d50f4a5cd89968f16571144b66b0c583a67bfec
rosedimple <rosedimplegeorge@gmail.com>
2018-03-28 11:10:21 -0400

    Created Array of  Objects for the Question-Answers

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 questionAnsArray = [
     {
-        qs11 : "How many Books are There in the Bible?",
-        ans1 : 100,
-        ans1 : 10,
-        ans1 : 80,
-        ans1 : 67
+        qestion11 : "How many Books are There in the Bible?",
+        ans11 : 100,
+        ans21 : 10,
+        ans31 : 80,
+        correctans11 : 67,
+        points :100
+    },
+    {
+        question12 :"What's the first book in the Bible",
+        ans21 : "Exodus",
+        ans22 : "Revelation",
+        ans23 : "pslams",
+        correctans12 : "Genesis",
+        points :100
+    },
+    {
+        question13 :"What's the last book in the Bible",
+        ans31 : "Exodus",
+        ans32 : "Esther",
+        ans33 : "pslams",
+        correctans13 : "Revelation",
+        points :100
+    },
+    {
+        question14 :"How many chapters are there inPsalms?",
+        ans41 : 160,
+        ans42 : 100,
+        ans43 : 99,
+        correctans14 : 150,
+        points :100
     }
+
 ];
